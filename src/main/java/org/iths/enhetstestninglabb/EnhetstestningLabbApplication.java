62f39205349943fd4502dde217eda66ca38ad8f0
AnwarAsry <anwareducation03@gmail.com>
2026-02-06 15:23:00 +0100

    init

---
+++ src/main/java/org/iths/enhetstestninglabb/EnhetstestningLabbApplication.java
@@ -0,0 +1,13 @@
+package org.iths.enhetstestninglabb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhetstestningLabbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnhetstestningLabbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iths/enhetstestninglabb/EnhetstestningLabbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iths.enhetstestninglabb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhetstestningLabbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
